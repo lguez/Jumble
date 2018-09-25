ac5d40f498cefe04dc014592bcee3a5f83d26a2d
Lionel GUEZ <guez@lmd.ens.fr>
2018-09-25 11:56:20 +0200

    Clarify procedure dtridgl. af(1), as(1) and cf(l) were useless. No need to declare a maximum size nmax.

--- Numerical/dtridgl.f
+++ Numerical/dtridgl.f
@@ -10,27 +10,33 @@ contains
     ! system of tridiagional matrix equations. The form of the
     ! equations are:
     ! A(I)*X(I-1) + B(I)*X(I) + C(I)*X(I+1) = D(I)
-    ! where i=1,l less than 103. Reviewed -CP
+    ! where i=1,l. Reviewed -CP
 
     integer, intent(in):: l
-    double precision, intent(in):: AF(L),BF(L),CF(L),DF(L)
+    double precision, intent(in):: AF(2:) ! (2:L)
+    double precision, intent(in):: BF(L),DF(L)
+    double precision, intent(in):: CF(:) ! (L - 1)
     double precision, intent(out):: XK(L)
 
     ! Variables local to the procedure:
 
-    integer, PARAMETER:: NMAX=201
-    double precision AS(NMAX),DS(NMAX),  xkb, x
+    double precision AS(2:l),DS(l),  xkb, x
     integer i
 
     !----------------------------
 
     AS(L) = AF(L)/BF(L)
     DS(L) = DF(L)/BF(L)
-    DO I=2,L
+    i = 2
+    
+    DO
        X=1d0/(BF(L+1-I) - CF(L+1-I)*AS(L+2-I))
-       AS(L+1-I)=AF(L+1-I)*X
        DS(L+1-I)=(DF(L+1-I)-CF(L+1-I)*DS(L+2-I))*X
+       if (i == l) exit
+       AS(L+1-I)=AF(L+1-I)*X
+       i = i + 1
     end DO
+    
     XK(1)=DS(1)
     DO I=2,L
        XKB=XK(I-1)
