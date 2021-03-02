d3a315111434de5c8d9183c7ce5db16234c16795
Lionel GUEZ <guez@lmd.ens.fr>
2021-03-02 22:06:11 +0100

    Create twin procedure for double precision
    Instead of specifying the precision with wp. This allows a using
    procedure to link to the single precision version of `NR_util`.

--- unit_matrix.f90
+++ unit_matrix.f90
@@ -2,15 +2,19 @@ module unit_matrix_m
 
   implicit none
 
-contains
-
-  SUBROUTINE unit_matrix(mat)
-
+  interface unit_matrix
     ! Sets matrix to be a unit matrix.
     
-    USE nrtype, only: wp
-    
-    REAL(WP), DIMENSION(:,:), INTENT(OUT) :: mat
+     module procedure unit_matrix_s, unit_matrix_d
+  end interface unit_matrix
+
+  private unit_matrix_s, unit_matrix_d
+
+contains
+
+  SUBROUTINE unit_matrix_s(mat)
+
+    REAL, DIMENSION(:,:), INTENT(OUT) :: mat
 
     ! Local:
     INTEGER i,n
@@ -18,12 +22,32 @@ contains
     !--------------------------------------------------------
     
     n=min(size(mat,1),size(mat,2))
-    mat(:,:)=0.0_wp
+    mat(:,:)=0.
 
     do i=1,n
-       mat(i,i)=1.0_wp
+       mat(i,i)=1.
     end do
 
-  END SUBROUTINE unit_matrix
+  END SUBROUTINE unit_matrix_s
+
+  !***************************************************************
+
+  SUBROUTINE unit_matrix_d(mat)
+
+    double precision, DIMENSION(:,:), INTENT(OUT) :: mat
+
+    ! Local:
+    INTEGER i,n
+
+    !--------------------------------------------------------
+    
+    n=min(size(mat,1),size(mat,2))
+    mat(:,:)=0d0
+
+    do i=1,n
+       mat(i,i)=1d0
+    end do
+
+  END SUBROUTINE unit_matrix_d
 
 end module unit_matrix_m
