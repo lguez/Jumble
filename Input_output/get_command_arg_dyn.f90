12f1f38c1cc1d5564c6edfd8e8a64440e2d3c82c
Lionel GUEZ <guez@lmd.ens.fr>
2023-10-20 23:40:40 +0200

    Add an interface to also read an integer value

--- Input_output/get_command_arg_dyn.f90
+++ Input_output/get_command_arg_dyn.f90
@@ -1,26 +1,37 @@
 module get_command_arg_dyn_m
 
+    use, intrinsic:: iso_fortran_env, only: error_unit
+
   implicit none
+
+  interface get_command_arg_dyn
+     ! This generic procedure is a wrapper for the intrinsic procedure
+     ! get_command_argument. The advantages over a direct call to
+     ! get_command_argument are:
+
+     ! - The arg_val character argument is automatically allocated to the
+     ! correct length.
+
+     ! - You do not have to check the return status:
+     ! get_command_arg_dyn aborts if there is a problem.
+
+     ! - You can directly read an integer value from an argument.
+
+     module procedure get_command_arg_dyn_char, get_command_arg_dyn_int
+  end interface get_command_arg_dyn
+
+  ! Note: no need to call command_argument_count first, because
+  ! there is probably no other reason for failure of the first call
+  ! to get_command_argument below than "number" being greater than
+  ! number of arguments.
+
+  private
+  public get_command_arg_dyn
 
 contains
 
-  subroutine get_command_arg_dyn(number, arg_val, message)
+  subroutine get_command_arg_dyn_char(number, arg_val, message)
 
-    ! This procedure is a wrapper for the intrinsic procedure
-    ! get_command_argument. The advantage over a direct call to
-    ! get_command_argument is that the arg_val argument is
-    ! automatically allocated to the correct length. Another
-    ! difference with a direct call to get_command_argument is that
-    ! you do not have to check the return status: get_command_arg_dyn
-    ! aborts if there is a problem.
-
-    ! Note: no need to call command_argument_count first, because
-    ! there is probably no other reason for failure of the first call
-    ! to get_command_argument below than "number" being greater than
-    ! number of arguments.
-
-    use, intrinsic:: iso_fortran_env
-    
     integer, intent(in):: number
     character(len = :), allocatable, intent(out):: arg_val
     character(len = *), intent(in), optional:: message
@@ -33,8 +44,8 @@ contains
     call get_command_argument(number, length = length, status = status)
 
     if (status /= 0) then
-       write(error_unit, fmt = *) "get_command_arg_dyn: number = ", number
-       write(error_unit, fmt = *) "get_command_arg_dyn: status = ", status
+       write(error_unit, fmt = *) "get_command_arg_dyn_char: number = ", number
+       write(error_unit, fmt = *) "get_command_arg_dyn_char: status = ", status
        if (present(message)) write(error_unit, fmt = *) message
        stop 1
     end if
@@ -42,6 +53,35 @@ contains
     allocate(character(len = length):: arg_val)
     call get_command_argument(number, arg_val)
 
-  end subroutine get_command_arg_dyn
+  end subroutine get_command_arg_dyn_char
+
+  !************************************************************************
+
+  subroutine get_command_arg_dyn_int(number, arg_val, message)
+
+    integer, intent(in):: number
+    integer, intent(out):: arg_val
+    character(len = *), intent(in), optional:: message
+
+    ! Local:
+    integer length, status
+    character(len = :), allocatable:: arg_val_char
+
+    !---------------------------------------------------------------------
+
+    call get_command_argument(number, length = length, status = status)
+
+    if (status /= 0) then
+       write(error_unit, fmt = *) "get_command_arg_dyn_int: number = ", number
+       write(error_unit, fmt = *) "get_command_arg_dyn_int: status = ", status
+       if (present(message)) write(error_unit, fmt = *) message
+       stop 1
+    end if
+
+    allocate(character(len = length):: arg_val_char)
+    call get_command_argument(number, arg_val_char)
+    read(unit = arg_val_char, fmt = *) arg_val
+
+  end subroutine get_command_arg_dyn_int
 
 end module get_command_arg_dyn_m
