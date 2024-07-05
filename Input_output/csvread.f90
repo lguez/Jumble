060b20fe8abb664b5fc2c0b0d617cd468ec811b7
Lionel GUEZ <guez@lmd.ipsl.fr>
2024-07-05 16:15:32 +0200

    Overload csvread with `csvread_int`

--- Input_output/csvread.f90
+++ Input_output/csvread.f90
@@ -12,23 +12,49 @@ module csvread_m
 
   interface csvread
      ! Reads numeric values from a file. Values must be separated by
-     ! comma and/or blanks. Values are read into a default real kind
-     ! or double precision array. The last column and/or last row
-     ! arguments may be 0. This is interpreted as "last in the
-     ! file". The only difference between the interfaces of the
+     ! comma and/or blanks. Values are read into an integer, default
+     ! real kind or double precision array. The last column and/or
+     ! last row arguments may be 0. This is interpreted as "last in
+     ! the file". The only difference between the interfaces of the
      ! specific procedures is the type of "a".
      
      ! character(len=*), intent(in):: file
-     ! real or double precision, allocatable, intent(out):: a(:,:)
+     ! integer, real or double precision, allocatable, intent(out):: a(:,:)
      ! integer, intent(in), optional:: first_r ! first row to read
      ! integer, intent(in), optional:: first_c ! first column to read
      ! integer, intent(in), optional:: last_r ! last row to read
      ! integer, intent(in), optional:: last_c ! last column to read
 
-     module procedure csvread_sp, csvread_dp
+     module procedure csvread_sp, csvread_dp, csvread_int
   end interface csvread
 
 contains
+
+  subroutine csvread_int(file, a, first_r, first_c, last_r, last_c, iostat)
+
+    character(len=*), intent(in):: file
+    integer, allocatable, intent(out):: a(:,:)
+    integer, intent(in), optional:: first_r
+    integer, intent(in), optional:: first_c
+    integer, intent(in), optional:: last_r
+    integer, intent(in), optional:: last_c
+    integer, intent(out), optional:: iostat
+
+    ! Variables local to the subprogram:
+    integer i, j, unit, iostat_loc
+    integer f_r_loc ! first row to read, local variable
+    integer f_c_loc ! first column to read, local variable
+    integer l_r_loc ! last row to read, local variable
+    integer l_c_loc ! last column to read, local variable
+    character trash, iomsg*200
+
+    !------------------------------------------------------
+
+    include "csvread.h"
+
+  end subroutine csvread_int
+
+  !***********************************************************
 
   subroutine csvread_sp(file, a, first_r, first_c, last_r, last_c, iostat)
 
