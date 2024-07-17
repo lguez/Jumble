9d4f666ed34db3b1fa9752c7e167f2df078a8ac9
Lionel GUEZ <guez@lmd.ipsl.fr>
2024-07-17 12:17:00 +0200

    Add program `test_squeeze`

---
+++ test_squeeze.f90
@@ -0,0 +1,15 @@
+program test_squeeze
+
+  use jumble, only: squeeze
+
+  implicit none
+
+  character(len = 100) line
+
+  !-----------------------------------------------------------------------
+
+  print *, "Enter a character string, with quotes:"
+  read *, line
+  write(unit = *, fmt = "(3a)") '"', squeeze(line, " "), '"'
+  
+end program test_squeeze
