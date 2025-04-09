39770ce6399cd53e1ac2c49081498358c44d9fd0
Lionel GUEZ <guez@lmd.ipsl.fr>
2025-04-09 15:14:19 +0200

    Polish

--- Input_output/new_unit.f90
+++ Input_output/new_unit.f90
@@ -7,18 +7,27 @@ contains
   subroutine new_unit(unit)
 
     ! This procedure provides a licit and not already opened external
-    ! file unit.
+    ! file unit. Note that if you call `new_unit` twice without
+    ! opening a file between the two calls, then `new_unit` wil return
+    ! the same value of `unit` for the two calls.
 
     integer, intent(out):: unit
 
-    ! Variables local to the procedure:
+    ! Local:
     logical opened, exist
 
     !------------------------------------------------------
 
     unit = 0
+
     do
-       inquire(unit=unit, opened=opened, exist=exist)
+       ! The collection of unit numbers that can be used in a program
+       ! for external files is determined by the processor. The unit
+       ! numbers that may be used are said to exist. There may be
+       ! certain unit numbers that are never allowed for user files
+       ! because they are restricted by the operating
+       ! system. Cf. Adams (2009, § 9.1.6.1).
+       inquire(unit = unit, opened = opened, exist = exist)
        if (exist .and. .not. opened) exit
        unit = unit + 1
     end do
